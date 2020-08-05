d1ac132e48e547d4c13549682e53ac83e280d4b6
Nishant Singh <saysnishant@gmail.com>
2020-08-05 14:43:34 +0530

    opengraph tags

--- components/PageHead.js
+++ components/PageHead.js
@@ -1,8 +1,15 @@
+import React from 'react'
 import Head from 'next/head'
 
 const PageHead = () => (
   <Head>
     <title>Nishant Singh</title>
+    <meta name="description" content="Founder at Ylurn.com" />
+    <meta property="og:title" content="Nishant Singh" />
+    <meta property="og:type" content="website" />
+    <meta property="og:url" content="https://nishant-singh.com" />
+    <meta property="og:image" content="https://nishant-singh.com/picture.png" />
+    <meta property="og:description" content="Founder at Ylurn.com" />
   </Head>
 )
 
