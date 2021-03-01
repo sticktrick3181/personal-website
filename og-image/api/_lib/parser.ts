6b0dc4f0e4bdb288b9dd62dfd732fa2fed09f414
Nishant Singh <saysnishant@gmail.com>
2021-03-01 21:05:25 +0530

    changed default image

--- og-image/api/_lib/parser.ts
+++ og-image/api/_lib/parser.ts
@@ -52,8 +52,8 @@ function getArray(stringOrArray: string[] | string | undefined): string[] {
 
 function getDefaultImages(images: string[], theme: Theme): string[] {
     const defaultImage = theme === 'light'
-        ? 'https://assets.vercel.com/image/upload/front/assets/design/vercel-triangle-black.svg'
-        : 'https://assets.vercel.com/image/upload/front/assets/design/vercel-triangle-white.svg';
+        ? 'https://nishant-singh.com/images/picture-circle.png'
+        : 'https://nishant-singh.com/images/picture-circle.png';
 
     if (!images || !images[0]) {
         return [defaultImage];
